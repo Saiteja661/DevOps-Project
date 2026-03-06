aa3cb7afbb2cb1926bf2028e5a2f1c4a5df8603c
sai <saiteja121020@gmail.com>
2026-03-06 17:21:12 -0500

    commiting code

--- 02-converation.sh
+++ 02-converation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # This is a simple script that prints "Hello, World!" to the console.
-echo "Teja: Hi there! How are you doing today?"
-echo "Assistant: I'm doing great, thank you! How can I assist you today?"
-echo "Teja: I have a question about DevOps. Can you explain what it is?"
-echo "Assistant: Of course! DevOps is a set of practices"
+Person1="Teja"
+Person2="Assistant"
+
+echo "$Person1: Hi there! How are you doing today?"
+echo "$Person2: I'm doing great, thank you! How can I assist you today?"
+echo "$Person1: I have a question about DevOps. Can you explain what it is?"
+echo "$Person2: Of course! DevOps is a set of practices"
